73266f09f74e68e999ae510a9035bf8574c1927d
Henrry Garrido <henrygarrido4@gmail.com>
2024-06-10 15:58:40 -0500

    updated gallery

--- js/glight.js
+++ js/glight.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const lightbox = GLightbox({
     selector: ".gallery_item a",
+    touchNavigation: true, // Enable touch navigation
+    loop: true, // Loop through the images
   });
 });
